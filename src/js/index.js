498fcd8d5aa3bd17537ed34d59df927fa4899e4d
Dima <webbootcamp2020@gmail.com>
2021-02-09 20:02:02 +0200

    Html and css selecting topic

--- src/js/index.js
+++ src/js/index.js
@@ -8,16 +8,35 @@ import '../sass/style.scss';
 
 class App extends Component {
 	static init() {
-		this.form = document.getElementById('visualizer-inputs-form');
-        this.form.addEventListener('submit', App.formSubmitHandler);
+		const visualizerSettigsForm = document.getElementById('visualizer-inputs-form');
+        visualizerSettigsForm.addEventListener('submit', App.formSubmitHandler);
+        App.initForms();
+	}
+
+	static initForms() {
+		const mobileTopicInput = document.querySelector('#mobile-topic select');
+		const topicForm = document.getElementById('topic-form');
+		mobileTopicInput.addEventListener('change', App.pickTopicHandler);
+	}
+
+	static pickTopicHandler(e) {
+		e.preventDefault();
+		if (e.target.closest('form').id === 'mobile-topic') {
+			this.topic = e.target.value;
+		}
 	}
 
 	static formSubmitHandler(e) {
 		e.preventDefault();
+		App.setup();
+	}
+
+	static setup() {
 		const prevArr = document.getElementById('visualizer-list');
 		const prevIndexes = document.getElementById('visualizer-indexes');
 		if (prevArr) prevArr.remove();
 		if (prevIndexes) prevIndexes.remove();
+
 		const arr = new Array('visualizer-canvas');
 		const controls = new VisualizerControls(new BubbleSort(arr));
 	}
